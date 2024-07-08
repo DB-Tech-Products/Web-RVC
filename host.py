769dac68114994e6dacbd0485ef1f8a53ea03d00
Brad H. <12530426+BradENGR@users.noreply.github.com>
2024-07-08 18:20:06 -0400

    reorg

--- host.py
+++ host.py
@@ -5,12 +5,11 @@ import os
 PORT = 8000
 
 # Define the directory to serve files from
-web_dir = os.path.join(os.path.dirname(__file__), 'www')
-os.chdir(web_dir)
+os.chdir(os.path.dirname(__file__))
 
 # Handler to serve files from the specified directory
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at port {PORT}")
+    print(f"Server started. Navigate to http://localhost:{PORT}/ in your web browser to view.")
     httpd.serve_forever()
