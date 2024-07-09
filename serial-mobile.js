346f9d55c581ef52fdfdf50e4a5b6251e5179b92
Brad H. <12530426+BradENGR@users.noreply.github.com>
2024-07-09 10:05:08 -0400

    mobile update

--- serial-mobile.js
+++ serial-mobile.js
@@ -1,17 +1,11 @@
-// serial-mobile.js
-import { serial as polyfill, SerialPort as SerialPortPolyfill } from 'https://unpkg.com/web-serial-polyfill';
+import { serial as polyfill } from 'https://unpkg.com/web-serial-polyfill';
 
 let port;
 let reader;
 let writer;
 
 export async function connect() {
-    if ('serial' in navigator) {
-        port = await navigator.serial.requestPort();
-    } else {
-        polyfill.apply();
-        port = await SerialPortPolyfill.requestPort();
-    }
+    port = await polyfill.requestPort();
     await port.open({ baudRate: 250000 });
 
     reader = port.readable.getReader();
@@ -27,8 +21,8 @@ export async function disconnect() {
     try {
         await writer.write(new TextEncoder().encode('C\r')); // Close CAN channel
         await reader.cancel(); // Cancel the reader to release it
-        await reader.releaseLock(); // Release the reader lock
-        await writer.close(); // Close the writer stream
+        reader.releaseLock(); // Release the reader lock
+        writer.releaseLock(); // Release the writer lock
         await port.close(); // Close the port
         console.log("Disconnected from serial port");
     } catch (error) {
@@ -41,16 +35,16 @@ export async function send(data) {
 }
 
 export async function readData(callback) {
-  const decoder = new TextDecoder();
-  try {
-    while (true) {
-      const { value, done } = await reader.read();
-      if (done) break;
-      const decodedData = decoder.decode(value);
-      console.log("Read data:", decodedData);
-      callback(decodedData);
+    const decoder = new TextDecoder();
+    try {
+        while (true) {
+            const { value, done } = await reader.read();
+            if (done) break;
+            const decodedData = decoder.decode(value);
+            console.log("Read data:", decodedData);
+            callback(decodedData);
+        }
+    } catch (error) {
+        console.error("Error reading data:", error);
     }
-  } catch (error) {
-    console.error("Error reading data:", error);
-  }
 }
